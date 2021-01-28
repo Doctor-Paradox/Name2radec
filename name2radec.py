30eaae19dd7cafcecb0a8c333cf8e840f4cb0ed1
Doctor-Paradox <78156703+Doctor-Paradox@users.noreply.github.com>
2021-01-28 20:03:12 +0530

    Add files via upload

---
+++ name2radec.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 18:34:36 2020
+
+@author: gurpreet
+"""
+import sys
+from astroquery.simbad import Simbad
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+# name=sys.argv[1]
+name=' '.join(sys.argv[1:])
+# name="capella"
+if (name == '-h' or name == '-i'):
+    print("----------------------------------------------------------------------------------------------------------------------------------")
+    print("This code takes name as input and find its R.A. and Dec. from Simbad using astroquery...")
+    print("Converts coordinates : hexadecimal to degrees")
+    print("E.g.                         name2radec capella")
+    print("----------------------------------------------------------------------------------------------------------------------------------")
+else:
+    result_table = Simbad.query_object(str(name)) 
+    ra=result_table["RA"]
+    dec=result_table["DEC"]
+    c =SkyCoord(str(ra.data[0])+' '+str(dec.data[0]), unit=(u.hourangle, u.deg))
+    print(result_table["MAIN_ID","RA","DEC"])
+    print(c)
+    c_icrs=SkyCoord(ra=c.ra*u.degree, dec=c.dec*u.degree, frame='icrs')
+    c_fk5 = c_icrs.transform_to('fk5')
+    print(c_fk5)
---
+++ name2radec_gui.sh
@@ -0,0 +1,17 @@
+a='-g'
+if [ -z "$1" ] 
+then echo "Please enter valid option..."
+echo "For GUI:"
+echo "name2radec -g "
+echo "For CLI:"
+echo "name2radec -c "
+elif [ $1 == $a ]
+then
+
+Response=$(zenity --entry --text "Enter your search term" --title "Name to R.A. Dec. converter" --entry-text="")
+(python /home/aries/bin/name2radec.py $Response || echo "ERROR: NAME NOT FOUND" )| zenity --text-info --title "Name to R.A. Dec. converter" #--width=100 --height=100
+else
+echo "Enter the name of object"
+read ans
+python /home/aries/bin/name2radec.py $ans || echo "ERROR: NAME NOT FOUND"
+fi
